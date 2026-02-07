3abfb395171cc7ff5e8b160773383693dcd727de
AtulMishra001 <atulm7151@gmail.com>
2026-02-07 20:43:03 +0530

    implemented database connection

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // Parses incoming JSON requests
+
+// Basic Route for testing
+app.get("/", (req, res) => {
+  res.send("YouTube Clone API is running...");
+});
+
+// connecting database
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err.message);
+  });
